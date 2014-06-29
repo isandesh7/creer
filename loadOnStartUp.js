00d9b5cb9692757af830e119ad56e411f921c2a0
isandesh7 <isandesh7@gmail.com>
2014-06-29 00:15:56 -0700

    Delete loadOnStartUp.js

--- loadOnStartUp.js
+++
@@ -1,6 +0,0 @@
-function loadScript(javaScriptFileName){
-    var jsLoad = document.createElement("loadScript");
-    jsLoad.type = "text/javascript"
-    jsLoad.src = javaScriptFileName;
-    document.head.appendChild(jsLoad);
-}
